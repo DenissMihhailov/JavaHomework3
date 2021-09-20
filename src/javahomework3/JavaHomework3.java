6ac5dda82ee12d6c02f14a596d1599f4fb1ec070
Your Name <krasava1081@gmail.com>
2021-09-21 01:38:26 +0300

    Third Update

--- src/javahomework3/JavaHomework3.java
+++ src/javahomework3/JavaHomework3.java
@@ -23,25 +23,70 @@ public class JavaHomework3 {
         System.out.println("Введите год:");
         int year= scanner.nextInt();
         int color=0;
+        int animal=0;
         
         switch(year%100) {
             case 84: case 85: case 86: case 87: case 88: case 89: case 90:  case 91: case 92: case 93: case 94: case 95: 
-            color=1;
+            color=1+color;
             case 96: case 97: case 98: case 99: case 00: case 01: case 02:  case 03: case 04: case 05: case 06: case 07: 
-            color=2;
+            color=2+color;
             case 8: case 9: case 10: case 11: case 12: case 13: case 14: case 15: case 16: case 17: case 18: case 19: 
-            color=3;
+            color=3+color;
             case 20: case 21: case 22: case 23: case 24: case 25: case 26: case 27: case 28: case 29: case 30: case 31: 
-            color=4;
+            color=4+color;
             case 32: case 33: case 34: case 35: case 36: case 39: case 40: case 41: case 42: case 43: case 44: case 45: 
-            color=5;
+            color=5+color;
             default:
-            color=6;
+            color=6+color;
             
-        switch(){
+        switch(animal) {
+            case 84: case 96: case 8: case 20: case 32: case 44: case 45:
+            animal=1+animal;
+            case 85: case 97: case 9: case 21: case 33:
+            animal=2+animal;
+            case 86: case 98: case 10: case 22: case 34:
+            animal=3+animal;
+            case 87: case 99: case 11: case 23: case 35:
+            animal=4+animal;
+            case 88: case 00: case 12: case 24: case 36:
+            animal=5+animal;
+            case 89: case 01: case 13: case 25: case 37:
+            animal=6+animal;
+            case 90: case 02: case 14: case 26: case 38:
+            animal=7+animal;
+            case 91: case 03: case 15: case 27: case 39:
+            animal=8+animal;
+            case 92: case 04: case 16: case 28: case 40:
+            animal=9+animal;
+            case 93: case 05: case 17: case 29: case 41:
+            animal=10+animal;
+            case 94: case 06: case 18: case 30: case 42:
+            animal=11+animal;
+            case 95: case 07: case 19: case 31: case 43:
+            animal=12+animal;
+            default:
+            animal=13+animal;
             
+            System.out.println("Год");
+            
+            if (color==1) {
+                System.out.println("зеленой");
+            if (color==2) {
+                System.out.println("красной");
+            if (color==3) {
+                System.out.println("Зеленой");
+            if (color==4) {
+                System.out.println("Зеленой");
+            if (color==5) {
+                System.out.println("Зеленой");
+            }
+            }
+            }
+            }
+            }
         }
     }
         
     
+}
 }
