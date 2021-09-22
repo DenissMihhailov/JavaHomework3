43f99e48504ca9fac4fef858836fd600964bc0dd
Your Name <you@example.com>
2021-09-23 02:45:44 +0300

    Fifth Update

--- src/javahomework3/JavaHomework3.java
+++ src/javahomework3/JavaHomework3.java
@@ -22,82 +22,73 @@ public class JavaHomework3 {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Введите год:");
         int year= scanner.nextInt();
+        System.out.println("Год");
         
-        
-        
-        switch(year%100) {
-            case 84: case 85: case 86: case 87: case 88: case 89: case 90:  case 91: case 92: case 93: case 94: case 95: 
-            color="зеленый";
-            case 96: case 97: case 98: case 99: case 00: case 01: case 02:  case 03: case 04: case 05: case 06: case 07: 
-            color="Красный";
+        switch(year) {
+            case 1984: case 1985: case 1986: case 1987: case 1988: case 1989: case 1990:  case 1991: case 1992: case 1993: case 1994: case 1995: 
+            System.out.println("Зеленой");
             break;
-            case 8: case 9: case 10: case 11: case 12: case 13: case 14: case 15: case 16: case 17: case 18: case 19: 
-            color="Желтый";
+            case 1996: case 1997: case 1998: case 1999: case 2000: case 2001: case 2002:  case 2003: case 2004: case 2005: case 2006: case 2007: 
+            System.out.println("Красной");
             break;
-            case 20: case 21: case 22: case 23: case 24: case 25: case 26: case 27: case 28: case 29: case 30: case 31: 
-            color="Белый";
+            case 2008: case 2009: case 2010: case 2011: case 2012: case 2013: case 2014: case 2015: case 2016: case 2017: case 2018: case 2019: 
+            System.out.println("Желтой");
             break;
-            case 32: case 33: case 34: case 35: case 36: case 39: case 40: case 41: case 42: case 43: case 44: case 45: 
-            color="Черный";
+            case 2020: case 2021: case 2022: case 2023: case 2024: case 2025: case 2026: case 2027: case 2028: case 2029: case 2030: case 2031: 
+            System.out.println("Белой");
+            break;
+            case 2032: case 2033: case 2034: case 2035: case 2036: case 2039: case 2040: case 2041: case 2042: case 2043: case 2044: case 2045: 
+            System.out.println("Черной");
             break;
             default:
-            color="";
+            System.out.println("*Программа неполной версии, только с 1984 до 2045*");
+        }
+        
+        switch(year) {
+            case 1984: case 1996: case 2008: case 2020: case 2032: case 2044: case 2045:
+            System.out.println("Крысы");
+            break; 
+            case 1985: case 1997: case 2009: case 2021: case 2033:
+            System.out.println("Коровы");
             break;
-            
-        switch(animal) {
-            case 84: case 96: case 8: case 20: case 32: case 44: case 45:
-            animal=1+animal;
-            case 85: case 97: case 9: case 21: case 33:
-            animal=2+animal;
-            case 86: case 98: case 10: case 22: case 34:
-            animal=3+animal;
-            case 87: case 99: case 11: case 23: case 35:
-            animal=4+animal;
-            case 88: case 00: case 12: case 24: case 36:
-            animal=5+animal;
-            case 89: case 01: case 13: case 25: case 37:
-            animal=6+animal;
-            case 90: case 02: case 14: case 26: case 38:
-            animal=7+animal;
-            case 91: case 03: case 15: case 27: case 39:
-            animal=8+animal;
-            case 92: case 04: case 16: case 28: case 40:
-            animal=9+animal;
-            case 93: case 05: case 17: case 29: case 41:
-            animal=10+animal;
-            case 94: case 06: case 18: case 30: case 42:
-            animal=11+animal;
-            case 95: case 07: case 19: case 31: case 43:
-            animal=12+animal;
+            case 1986: case 1998: case 2010: case 2022: case 2034:
+            System.out.println("Тигры");
+            break;
+            case 1987: case 1999: case 2011: case 2023: case 2035:
+            System.out.println("Зайки"); 
+            break;
+            case 1988: case 2000: case 2012: case 2024: case 2036:
+            System.out.println("Дракоши");
+            break;
+            case 1989: case 2001: case 2013: case 2025: case 2037:
+            System.out.println("Змеи");
+            break;
+            case 1990: case 2002: case 2014: case 2026: case 2038:
+            System.out.println("Лошади");
+            break;
+            case 1991: case 2003: case 2015: case 2027: case 2039:
+            System.out.println("Овцы");
+            break;
+            case 1992: case 2004: case 2016: case 2028: case 2040:
+            System.out.println("Обезъяны");
+            break;
+            case 1993: case 2005: case 2017: case 2029: case 2041:
+            System.out.println("Курицы");
+            break;
+            case 1994: case 2006: case 2018: case 2930: case 2042:
+            System.out.println("Собаки");
+            break;
+            case 1995: case 2007: case 2019: case 2031: case 2043:
+            System.out.println("Свиньи");
+            break;
             default:
-            animal=13+animal;
+            System.out.println("*Программа неполной версии, только с 1984 до 2045*");;
             
-            System.out.println("Год");
-            System.out.println(color);
+
             
             
-            if (color==1) {
-                System.out.println("зеленой");
-            if (color==2) {
-                System.out.println("красной");
-            if (color==3) {
-                System.out.println("желтой");
-            if (color==4) {
-                System.out.println("белой");
-            if (color==5) {
-                System.out.println("черной");
-            if(color==6){
-                System.out.println("Ошибка");
-            
-            }
-            }
-            }
-            }
-            }
-            }
         }
     }
         
     
-}
 }
